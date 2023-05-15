cfae857cc1878f8cf6481b12d07d291256901b63
Francisco Masera <francisco.masera@externos.bci.cl>
2023-05-15 17:11:40 -0300

    exercises

---
+++ src/java-prom-path/threads/src/MultiExecutor.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Ejercicio realzado en curso udemy
+ *
+ * @url: https://globallogic.udemy.com/course/java-multithreading-concurrency-performance-optimization
+ */
+public class MultiExecutor {
+
+    private List<Runnable> tasks;
+
+    /*
+     * @param tasks to executed concurrently
+     */
+    public MultiExecutor(List<Runnable> tasks) {
+        // Complete your code here
+        this.tasks = tasks;
+    }
+
+    public static void main(String[] args) {
+        var main = new MultiExecutor(null);
+        main.getTasks();
+        main.executeAll();
+    }
+
+    private void getTasks() {
+        tasks = new ArrayList<Runnable>();
+        for (int i = 0; i < 4; i++) {
+            tasks.add(() -> System.out.println(Thread.currentThread().getName()));
+        }
+    }
+
+    /**
+     * Starts and executes all the tasks concurrently
+     */
+    public void executeAll() {
+        // complete your code here
+        List<Thread> threads = new ArrayList<>(tasks.size());
+        for (Runnable task : tasks) {
+            Thread thread = new Thread(task);
+            threads.add(thread);
+        }
+        for (Thread thread : threads) {
+            thread.start();
+        }
+    }
+}
