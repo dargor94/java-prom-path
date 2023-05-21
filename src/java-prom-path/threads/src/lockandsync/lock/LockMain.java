94928d05751e4455bc75e7b3e99b94917e16dbd3
Dargor94 <franciscomasera1994@gmail.com>
2023-05-21 00:59:35 -0300

    sync and lock

---
+++ src/java-prom-path/threads/src/lockandsync/lock/LockMain.java
@@ -0,0 +1,13 @@
+package lockandsync;
+
+public class LockMain implements Runnable {
+    public static void main(String[] args) {
+        var main = new LockMain();
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
