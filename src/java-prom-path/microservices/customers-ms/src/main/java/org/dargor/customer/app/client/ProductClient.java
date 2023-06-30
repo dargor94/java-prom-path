e1aa15c5d9ed49dd399e256cde728b8f39c3ad28
Dargor94 <franciscomasera1994@gmail.com>
2023-06-29 23:01:36 -0300

    end

--- src/java-prom-path/microservices/customers-ms/src/main/java/org/dargor/customer/app/client/ProductClient.java
+++ src/java-prom-path/microservices/customers-ms/src/main/java/org/dargor/customer/app/client/ProductClient.java
@@ -2,6 +2,7 @@ package org.dargor.customer.app.client;
 
 import org.dargor.customer.app.dto.ProductDto;
 import org.dargor.customer.app.dto.WishListDto;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,13 +12,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import java.util.List;
 import java.util.UUID;
 
-@FeignClient(name = "ProductClient", url = "http://localhost:9001/product")
+@FeignClient(name = "ProductClient", url = "http://localhost:8080/product-ms/product")
 public interface ProductClient {
 
+    @LoadBalanced
     @GetMapping("/wish-list/{customerId}")
     List<ProductDto> getWishList(@PathVariable UUID customerId);
 
-
+    @LoadBalanced
     @PostMapping("/create")
     WishListDto createProducts(@RequestBody WishListDto products);
 
