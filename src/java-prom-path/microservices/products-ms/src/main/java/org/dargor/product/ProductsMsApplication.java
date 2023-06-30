b60d9ae0c5d4283f1ad5f54073c78d45db95e1d4
Dargor94 <franciscomasera1994@gmail.com>
2023-06-29 23:01:42 -0300

    end

--- src/java-prom-path/microservices/products-ms/src/main/java/org/dargor/product/ProductsMsApplication.java
+++ src/java-prom-path/microservices/products-ms/src/main/java/org/dargor/product/ProductsMsApplication.java
@@ -2,8 +2,11 @@ package org.dargor.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableDiscoveryClient
 @EnableJpaRepositories
 @SpringBootApplication
 public class ProductsMsApplication {
