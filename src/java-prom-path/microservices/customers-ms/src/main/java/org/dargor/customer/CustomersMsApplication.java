5cd9830da9cb99cf5f2ea5ff11acd6c1a726f2c7
Dargor94 <franciscomasera1994@gmail.com>
2023-07-01 20:30:17 -0300

    config

--- src/java-prom-path/microservices/customers-ms/src/main/java/org/dargor/customer/CustomersMsApplication.java
+++ src/java-prom-path/microservices/customers-ms/src/main/java/org/dargor/customer/CustomersMsApplication.java
@@ -2,11 +2,11 @@ package org.dargor.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableFeignClients
 @EnableJpaRepositories
 @SpringBootApplication
